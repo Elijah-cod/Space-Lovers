f979e1a610699dacc123a896b6b8685eacfe4f5f
Elijah <iamngunjiri@gmail.com>
2024-12-20 04:55:27 +0300

    The search function is working and responsive

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const apod = document.getElementById('apod')
 const search = document.getElementById('search')
 const input = document.getElementById('input')
 const result = document.getElementById('result')
+const homeTab = document.getElementById('home-tab');
+const apodTab = document.getElementById('apod-tab');
+
 
 //Display random images every time the page loads
 window.onload = async function () {
@@ -96,14 +99,12 @@ function moreDetails(element) {
 
 //Function to switch to the different tabs
 function switchTab(tab) {
-    // Select the tab elements
-    const homeTab = document.getElementById('home-tab');
-    const apodTab = document.getElementById('apod-tab');
-
     if (tab === 'home') {
         // Show Home content and hide Discover content
         home.classList.remove('hide');
         apod.classList.add('hide');
+        result.classList.add('hide')
+
 
         // Set active tab
         homeTab.classList.add('active');
@@ -113,6 +114,8 @@ function switchTab(tab) {
         // Show Discover content and hide Discover content
         apod.classList.remove('hide');
         home.classList.add('hide');
+        result.classList.add('hide')
+
 
         // Set active tab
         apodTab.classList.add('active');
@@ -121,16 +124,16 @@ function switchTab(tab) {
 }
 
 //Function to display the Astronomy Picture of the Day
-function pictureOfTheDay () {
+function pictureOfTheDay() {
     //Nasa APOD url API
     const url = "https://api.nasa.gov/planetary/apod?api_key=uh5Hr2gPTUW6ZznWiHgghsswiDaRbO7NbymLe4bt"
 
     fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        //Clear the previous data because it updates the POD everyday
-        apod.innerHTML = " "
-        apod.innerHTML = `
+        .then(response => response.json())
+        .then(data => {
+            //Clear the previous data because it updates the POD everyday
+            apod.innerHTML = " "
+            apod.innerHTML = `
             <div class="heading"> 
             <img src=${data.url} alt=${data.title}>
             <h1>Astronomy Picture of the Day</h1>
@@ -140,14 +143,17 @@ function pictureOfTheDay () {
             <p>${data.explanation}</p>
             <p class="date"><strong>DATE: </strong>${data.date}</p>
             `
-    })
+        })
 }
 
 //Function to capture the input item and use it for search and then update the DOM
-search.addEventListener('click', ()=>{
+search.addEventListener('click', () => {
     result.classList.remove('hide')
-    home.classList.add('hide');
-    apod.classList.add('hide');
+    home.classList.add('hide')
+    apod.classList.add('hide')
+
+    homeTab.classList.remove('active');
+    apodTab.classList.remove('active');
 
     const searchItem = input.value
 
@@ -156,7 +162,68 @@ search.addEventListener('click', ()=>{
         const url = `https://images-api.nasa.gov/search?q=${searchItem}`
 
         fetch(url)
-        .then(response => response.json())
-        .then(data => console.log(data))
-    } 
+            .then(response => response.json())
+            .then(data => {
+                displaySearch(data.collection.items)
+            })
+    }
+    input.value = " "
 })
+
+//Function to update the DOM with the search results
+function displaySearch(data) {
+    result.innerHTML = '';  // Clear any existing content in the result
+    data.forEach(item => {
+        //Condition to determine which format to use to display content on the DOM
+        if (item.data[0].media_type === "video") {
+            fetch(item.href)
+                .then(response => response.json())
+                .then(video => {
+                    //Passing the video links and the items
+                    createVideoPlayer(video, item)
+                })
+        }
+    })
+}
+
+//Function to allow for the users to play the video in the search
+function createVideoPlayer(vids, item) {
+    //Retuns a list of video links
+    const videoLink = vids.filter(data => data.endsWith('.mp4'))
+    
+    //Create a card for the videos
+    const card = document.createElement('div')
+    card.classList.add('card')
+    card.style = "width: 640px;"
+
+    // Create a video element
+    const video = document.createElement('video');
+    video.src = videoLink[0];
+    video.width = 640;  // Set the video width (optional)
+    video.controls = true;  // Enable video controls (play, pause, volume)
+
+    card.append(video)
+
+    //Creating the card body and adding it to the card
+    const cardBody = document.createElement('div')
+    cardBody.classList.add('card-body')
+
+    //Getting the card title
+    const cardTitle = document.createElement('h5')
+    cardTitle.classList.add('card-title')
+    cardTitle.textContent = item.data[0].title
+    cardBody.append(cardTitle)
+
+    //Getting the descritpion
+    const cardText = document.createElement('p')
+    cardText.classList.add("card-text")
+    cardText.textContent =  item.data[0].description
+    cardBody.append(cardText)
+
+    //Adding the card elements to the card
+    card.append(cardBody)
+
+    //Adding the card to the search result container
+    result.append(card);
+}
+
